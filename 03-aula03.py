d3df3c5f63be246710912069e3009331d3a30149
Rafael Batista <rafa.nm100@gmail.com>
2024-08-29 11:48:13 -0300

    Add files via upload

---
+++ 03-aula03.py
@@ -0,0 +1,16 @@
+# Variáveis em Python
+# Não ha necessidade de declará-las no início do programa
+# Basta criar no meio do algoritmo com a atribuição
+
+teste = "Rafael" # conteudo com sinal de igual(=)
+print(teste) #imprime a string "Rafael"
+print(type(teste)) # imprime o tipo da variável, que é float
+teste = 10
+print(teste) #imprime o numero 10
+print(type(teste)) # imprime o tipo da variável, que é float
+teste = 1.74
+print(teste) #imprime o numero 1.74
+print(type(teste)) # imprime o tipo da variável, que é float
+teste = True
+print(teste) #imprime o valor true
+print(type(teste)) # imprime o tipo da variável, que é float
---
+++ 04-nome-idade.py
@@ -0,0 +1,15 @@
+# Atribuição de variáveis
+nome = "Rafael"
+idade = 16
+altura = 1.83
+
+# Exibir mensagem na tela
+print("nome: ", nome, " idade: ", idade, " altura: ", altura)
+# formatação com f-string
+print(f"nome: {nome} idade:{idade} altura: {altura}")
+
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite o sua idade: "))
+altura = float(input("Digite o sua altura: "))
+
+print(f"nome: {nome} idade:{idade} altura: {altura}")
