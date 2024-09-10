1237bc9b2d5bb348d672b07d39b19d42fa67295d
Rafael Batista <rafa.nm100@gmail.com>
2024-09-10 10:42:21 -0300

    Add files via upload

---
+++ 05-tipos-dados.py
@@ -0,0 +1,13 @@
+# Tipos de dados em Python
+# Inteiros
+inteiro = 10
+print(inteiro)
+# Floats
+float = 10.5
+print(float)
+# Strings
+string = "Olá, mundo!"
+print(string)
+# Booleanos
+booleano = True
+print(booleano)
