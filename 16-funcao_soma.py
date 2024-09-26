e0dbb2fd6abeaf9506fd7f9fa51f20c2c73b6ca0
Rafael Batista <rafa.nm100@gmail.com>
2024-09-26 11:51:15 -0300

    Add files via upload

---
+++ 16-funcao_soma.py
@@ -0,0 +1,12 @@
+# Declaração da função somar
+def somar(a, b):
+    return a + b
+
+
+# Algoritmo da soma
+print("Algoritmo de Soma")
+valor1 = int(input("Digite o valor 1: "))
+valor2 = int(input("Digite o valor 2: "))
+resultado = somar(valor1, valor2)
+
+print(f"O resultado da soma = {resultado}")
