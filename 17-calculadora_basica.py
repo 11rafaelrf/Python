d214ba610de70b51cf117460b8dc2007acb169c7
Rafael Batista <rafa.nm100@gmail.com>
2024-10-02 11:20:06 -0300

    Add files via upload

--- 17-calculadora_basica.py
+++ 17-calculadora_basica.py
@@ -1,7 +1,28 @@
-#Calculadora das operações básicas
-opcao = 0
-while opcao != 6:
-    print("CALCULADORA DAS OPERAÇÕES BÁSICAS: ")
+#Definição das funções
+def soma(n1, n2):
+    return n1 + n2
+
+def subtracao(n1, n2):
+    return n1 - n2
+
+def multiplicacao(n1, n2):
+    return n1 * n2
+
+def divisao(n1, n2):
+    if n2 != 0:
+        return n1 / n2
+    else:
+        return "Erro: Divisão por zero não é permitida."
+
+def resto_divisao(n1, n2):
+    if n2 != 0:
+        return n1 % n2
+    else:
+        return "Erro: Divisão por zero não é permitida."
+
+#Função exibir menu
+def exibirMenu():
+    print("CALCULADORA DAS OPERAÇÕES BÁSICAS:")
     print("Menu de escolha")
     print("1. Soma")
     print("2. Subtração")
@@ -9,48 +30,35 @@ while opcao != 6:
     print("4. Divisão")
     print("5. Resto da Divisão")
     print("6. Sair")
+    escolha = int(input("Escolha uma opção: "))
+    return escolha
 
-    opcao = int(input("Digite uma opção: "))
+#Declaração de variáveis
+opcao = 0
+n1 = 0
+n2 = 0
+
+while opcao != 6:
+    opcao = exibirMenu()
+    
+    if opcao in [1, 2, 3, 4, 5]:
+        n1 = float(input("Insira o primeiro número: "))
+        n2 = float(input("Insira o segundo número: "))
+
     
     if opcao == 1:
-       num1 = float(input("Digite o primeiro número: "))
-       num2 = float(input("Digite o segundo número: "))
-       resultado = num1 + num2
-       print(f"O resultado da soma é: {resultado}")
+       print(f"A soma dos valores é: {soma(n1, n2)}\n")
 
     elif opcao == 2:
-       num1 = float(input("Digite o primeiro número: "))
-       num2 = float(input("Digite o segundo número: "))
-       resultado = num1 - num2
-       print(f"O resultado da subtração é: {resultado}")
-
+       print(f"A subtração dos valores é: {subtracao(n1, n2)}\n")
     elif opcao == 3:
-       num1 = float(input("Digite o primeiro número: "))
-       num2 = float(input("Digite o segundo número: "))
-       resultado = num1 * num2
-       print(f"O resultado da multiplicação é: {resultado}")
-
+       print(f"A multiplicação dos valores é: {multiplicacao(n1, n2)}\n")
     elif opcao == 4:
-            num1 = float(input("Digite o primeiro número: "))
-            num2 = float(input("Digite o segundo número: "))
-            if num2 != 0:
-                resultado = num1 / num2
-                print(f"Resultado da divisão: {resultado}")
-            else:
-                print("Erro: Divisão por zero não é permitida.")
-
+       print(f"A divisão dos valores é: {divisao(n1, n2)}\n")
     elif opcao == 5:
-            num1 = float(input("Digite o dividendo: "))
-            num2 = float(input("Digite o divisor: "))
-            if num2 != 0:
-                resultado = num1 % num2
-                print(f"Resultado do resto da divisão: {resultado}")
-            else:
-                print("Erro: Divisor não pode ser zero.")
-      
+       print(f"O resto da divisão dos valores é: {resto_divisao(n1, n2)}\n") 
     elif opcao == 6:
-        print("Saindo...")
-        
+        print("Finalizando o código!")   
     else:
-        print("Opção Inválida")
+        print("Opção Inválida. Tente novamente.\n")
     
