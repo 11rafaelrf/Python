f3e6cd9f2c13e6cf89043d8df6acc996c137875f
Rafael Batista <rafa.nm100@gmail.com>
2024-10-03 11:40:18 -0300

    Add files via upload

---
+++ 00-instruções.py
@@ -0,0 +1,41 @@
+# print = escreva ou mostrar
+
+# teste = "Rafael" # conteudo com sinal de igual(=)
+# print(teste) #imprime a string "Rafael" = declaração de variável
+# teste = 10
+# print(teste) #imprime o numero 10
+
+# type = retorna a classe atribuida
+
+# print(type(teste)) # imprime o tipo da variável, que é float
+# teste = 1.74
+# print(teste) #imprime o numero 1.74
+# print(type(teste)) # imprime o tipo da variável, que é float
+# teste = True
+# print(teste) #imprime o valor true
+# print(type(teste)) # imprime o tipo da variável, que é float
+
+# Atribuição de variáveis
+# nome = "Rafael"
+# idade = 16
+# altura = 1.83
+
+# Exibir mensagem na tela
+# print("nome: ", nome, " idade: ", idade, " altura: ", altura)
+# formatação com f-string
+# print(f"nome: {nome} idade:{idade} altura: {altura}")
+
+# input = leia ou input(digite o seu nome: )
+# nome = input("Digite o seu nome: ")
+# idade = int(input("Digite o sua idade: "))
+# altura = float(input("Digite o sua altura: "))
+# print(f"nome: {nome} idade:{idade} altura: {altura}")
+
+# while = enquanto-repetição/ repete ate dar o valor aceitavel
+
+# if (se)
+# if - else (se - senão) / else 
+# if - elif - else (se - senão se - senão) / else para opção inválida
+
+# def = definir a função ou declarar exemplo: def somar(a, b)/ se inicia apos a declaração da função
+# return = retorno / leva o resultado e armazena a soma
